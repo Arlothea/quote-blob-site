7c174af69c00eac50c2e4e99e1bc3c3ccc655d1c
Arlothea <Adam.s.wingell@gmail.co.uk>
2026-03-05 13:57:28 +0000

    Add modular formatter router

---
+++ api/src/formatters/index.ts
@@ -0,0 +1,8 @@
+export type FormatterAction = "uppercase" | "sentencecase";
+export type FormatterFn = (input: string) => string;
+import { uppercase } from "./uppercase";
+import { sentencecase } from "./sentencecase";
+export const FORMATTERS: Record<FormatterAction, FormatterFn> = {
+    uppercase,
+    sentencecase
+};
